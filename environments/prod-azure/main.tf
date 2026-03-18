297df64673a51ef606b3de7abbdc8b40d33ef8c2
eli <iiyi>
2026-03-18 18:58:08 +0100

    change control center

--- environments/prod-azure/main.tf
+++ environments/prod-azure/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "acme" {
     Environment = var.environment
     ManagedBy   = "terraform"
     Initiative  = "cloud2"
-    CostCenter  = "cost control2"
+    CostCenter  = "finance"
   }
 }
 
